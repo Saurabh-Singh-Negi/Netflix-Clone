1bc60170a77c3617168b0d0ec4313d93d0f05b85
Saurabh <saurabhnegi0007@gmail.com>
2022-10-21 00:34:07 +0530

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import requests from '../Requests';
+
+const Main = () => {
+    const {movies, setMovies} = useState([]);
+
+    useEffec
+
+    return (
+        <div>
+            Main
+        </div>
+    )
+}
+
+export default Main;
