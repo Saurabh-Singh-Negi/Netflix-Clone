8651e79458af5aea91043995f413170ca6bb10d8
Saurabh <saurabhnegi0007@gmail.com>
2022-10-21 14:38:15 +0530

    added scroll functionality to Row Component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -12,14 +12,14 @@ const Movie = ({item}) => {
       />
 
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 text-white  opacity-0 hover:opacity-100">
-        <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+        <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
           {item?.title}
         </p>
         <p>
           {like ? (
-            <FaHeart className="absolute top-4 left-4" />
+            <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
-            <FaRegHeart className="absolute top-4 left-4" />
+            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
           )}
         </p>
       </div>
