75601021ec087f145838d5439604b18a64c27c72
Saurabh <saurabhnegi0007@gmail.com>
2022-10-21 22:51:45 +0530

    added styling for Sign Up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
 const Navbar = () => {
-    return (
-        <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-            <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
-            <div>
-                <button className="text-white pr-4">Sign In</button>
-                <button className="bg-red-600 px-6 py-2 rounded pointer text-white">Sign Up</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      <div>
+        <Link to="Login">
+          <button className="text-white pr-4">Sign In</button>
+        </Link>
+
+        <Link to="Signup">
+          <button className="bg-red-600 px-6 py-2 rounded pointer text-white">
+            Sign Up
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
