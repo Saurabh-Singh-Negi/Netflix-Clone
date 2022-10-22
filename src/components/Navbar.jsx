1aa5ffe9e61a5137a5f5759c20705e9a9625eac0
Saurabh <saurabhnegi0007@gmail.com>
2022-10-22 18:28:07 +0530

    connected firebase with the signUp page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
@@ -9,17 +12,32 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
-      <div>
-        <Link to="Login">
-          <button className="text-white pr-4">Sign In</button>
-        </Link>
 
-        <Link to="Signup">
-          <button className="bg-red-600 px-6 py-2 rounded pointer text-white">
-            Sign Up
-          </button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
+          </Link>
+
+          <Link to="Signup">
+            <button className="bg-red-600 px-6 py-2 rounded pointer text-white">
+              Sign Out
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
