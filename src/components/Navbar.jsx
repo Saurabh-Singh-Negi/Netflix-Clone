e6cd95754fd62a86c914eae90d1db5a244088000
Saurabh <saurabhnegi0007@gmail.com>
2022-10-23 23:56:46 +0530

    connected firebase with login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,  useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
@@ -18,12 +28,11 @@ const Navbar = () => {
           <Link to="/account">
             <button className="text-white pr-4">Account</button>
           </Link>
-
-          <Link to="Signup">
-            <button className="bg-red-600 px-6 py-2 rounded pointer text-white">
-              Sign Out
-            </button>
-          </Link>
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 px-6 py-2 rounded pointer text-white">
+            Sign Out
+          </button>
         </div>
       ) : (
         <div>
